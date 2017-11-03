fb287e80d30bfbd16964983b2ec3abe0da36c12f
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2017-11-03 12:47:19 +0545

    Update tt_match table

--- sports-backend/src/migrations/20171103104703_create_tt_match_table.ts
+++ sports-backend/src/migrations/20171103104703_create_tt_match_table.ts
@@ -1,4 +1,4 @@
-import * as Knex from "knex";
+import * as Knex from 'knex';
 
 exports.up = async (knex: Knex) => {
   return knex.schema.createTable('tt_match', (table: Knex.CreateTableBuilder) => {
@@ -8,6 +8,7 @@ exports.up = async (knex: Knex) => {
     table.integer('round_id').notNullable().unsigned().references('round.id');
     table.integer('player_1_id').nullable().unsigned().references('player.id');
     table.integer('player_2_id').nullable().unsigned().references('player.id');
+    table.dateTime('fixture_date').nullable();
 
     table.dateTime('created_at').defaultTo(knex.fn.now());
     table.dateTime('updated_at').defaultTo(knex.fn.now());
