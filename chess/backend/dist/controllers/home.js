5f817d1a837d87409125839f504be3fbc4eb7f11
Sanjeev Kumar Pandit <sanjeevpandit@lftechnology.com>
2018-01-17 21:49:04 +0545

    Remove dist

--- chess/backend/dist/controllers/home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = (req, res) => {
-    return res.json({
-        message: 'Test successful'
-    });
-};
-//# sourceMappingURL=home.js.map
--- chess/backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const HomeController = require("../controllers/home");
-const router = express_1.Router();
-router.get('/', HomeController.index);
-exports.default = router;
-//# sourceMappingURL=index.js.map
