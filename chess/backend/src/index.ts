8cc6117115eb24e3808abf89b6a21f03ab0839dd
Sagar Chamling <sgr.raee@gmail.com>
2018-01-21 22:46:37 +0545

    SPORTS-10 Add cors and helmet middlewares

--- chess/backend/src/index.ts
+++ chess/backend/src/index.ts
@@ -1,14 +1,18 @@
-import config from './config/app';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 import routes from './routes';
+import config from './config/app';
 import notFoundHandler from './middlewares/notFoundHandler';
 import genericErrorHandler from './middlewares/genericErrorHandler';
 
 const { port } = config;
 const app: express.Application = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
