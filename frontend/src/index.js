35db5fc1aafa60d36942d46881529b2dd22d954e
Arun Tamang <aruntamang3791@gmail.com>
2018-02-16 01:33:51 +0545

    SPORTS-44 Write basic markup for admin-login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,22 @@ import logo from '../public/images/logo-transparent.png';
 export default class Hello extends Component {
   render() {
     return (
-      'hello world'
+      <div className="login-outer-container">
+        <div className="login-inner-container">
+          <div className="login-header">
+            <a href="#">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <form className="login-form">
+            <div className="input-group">
+              <input type="email" placeholder="EMAIL" name="email" required={true} />
+              <input type="password" placeholder="PASSWORD" name="psw" required={true} />
+            </div>
+            <button type="submit">SIGN IN</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
