ba4e6bce4c9acf8d31cdac3aff4e15c1de2d8a5f
Arun Tamang <aruntamang3791@gmail.com>
2018-02-16 17:30:41 +0545

    SPORTS-33 Fix length rule in file imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import { render } from 'react-dom';
 import React, { Component } from 'react';
+
 import Routes from './components/Routes';
 
-import '../public/assets/css/style.css';
 import './assets/sass/style.scss';
+import '../public/assets/css/style.css';
 
 export default class Hello extends Component {
   render() {
