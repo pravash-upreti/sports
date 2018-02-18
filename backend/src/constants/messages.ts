99bfa56fa80f4762b8e2fc8df6d5fa52cff00d4e
Sanjeev Kumar Pandit <sanjeevpandit@lftechnology.com>
2018-02-18 20:46:04 +0545

    SPORTS-47 Correct typo

--- backend/src/constants/messages.ts
+++ backend/src/constants/messages.ts
@@ -3,7 +3,7 @@ export const tournamentMessages = {
   'created': 'Tournament created successfully.',
   'updated': 'Tournament updated successfully.',
   'removed': 'Tournament deleted successfully.',
-  'unableToRemove': 'Unable to remoce the tournament.',
+  'unableToRemove': 'Unable to remove the tournament.',
   'unableToUpdate': 'Unable to update the tournament.',
   'unableToCreate': 'Unable to create a new tournament.'
 };
