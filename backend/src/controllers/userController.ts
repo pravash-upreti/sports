1938b353c4b6292d5c7dd5c3cd336ae477ba2796
Arun Tamang <aruntamang3791@gmail.com>
2018-02-21 23:26:01 +0545

    SPORTS-52 Add new line before response in userController

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -13,6 +13,7 @@ import * as UserService from '../services/userService';
 export async function getAll(req: Request, res: Response, next: NextFunction) {
   try {
     const response = await UserService.getAll();
+
     res.status(response.code).json(response);
   } catch (error) {
     next(error);
@@ -30,6 +31,7 @@ export async function getAll(req: Request, res: Response, next: NextFunction) {
 export async function create(req: Request, res: Response, next: NextFunction) {
   try {
     const response = await UserService.create(req.body);
+
     res.status(response.code).json(response);
   } catch (error) {
     next(error);
@@ -47,6 +49,7 @@ export async function create(req: Request, res: Response, next: NextFunction) {
 export async function createRole(req: Request, res: Response, next: NextFunction) {
   try {
     const response = await UserService.createRole(req.body);
+
     res.status(response.code).json(response);
   } catch (error) {
     next(error);
