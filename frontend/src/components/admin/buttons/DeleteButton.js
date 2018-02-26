592b524277d7a48c2882598c4783e4baca88d538
ishanliv99 <ishanliv99@gmail.com>
2018-02-26 10:20:30 +0545

    SPORTS-55 Complete functionalities for Tournament CRUD

--- frontend/src/components/admin/buttons/DeleteButton.js
+++ frontend/src/components/admin/buttons/DeleteButton.js
@@ -1,8 +1,34 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
 
-const DeleteButton = props => {
-  return <button type="submit">Delete</button>;
-};
+function DeleteData(props) {
+  const { id, tournament, updateTournament } = props;
 
-export default DeleteButton;
+  const remove = () => {
+    axios
+      .delete("http://0.0.0.0:5000/api/v1/tournaments/" + id)
+      .then(res => {
+        axios
+          .get("http://0.0.0.0:5000/api/v1/tournaments")
+          .then(res => {
+            const tournament = (res && res.data && res.data.data) || [];
+            updateTournament(tournament);
+          })
+          .catch(err => err);
+      })
+      .catch(err => err);
+  };
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        remove();
+      }}
+    >
+      <button>Delete</button>
+    </form>
+  );
+}
+
+export default DeleteData;
--- frontend/src/components/admin/buttons/EditButton.js
+++ frontend/src/components/admin/buttons/EditButton.js
@@ -1,8 +1,91 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
+import moment from "moment";
+import InputMask from "react-input-mask";
 
-const EditButton = props => {
-  return <button type="submit">Edit</button>;
-};
+function EditData(props) {
+  const {
+    id,
+    title,
+    startDate,
+    finishDate,
+    tournament,
+    isEditable,
+    handleEditClick,
+    updateTournament,
+    handleChangeTitle,
+    handleChangeStartDate,
+    handleChangeFinishDate
+  } = props;
 
-export default EditButton;
+  const edit = () => {
+    if (
+      moment(startDate, "YYYY/MM/DD").isValid &&
+      (finishDate === null || moment(finishDate, "YYYY/MM/DD").isValid)
+    ) {
+      axios
+        .put("http://0.0.0.0:5000/api/v1/tournaments/" + id, {
+          title: "title",
+          start_date: "2012/12/12",
+          finish_date: "2013/12/12"
+        })
+        .then(res => {
+          axios
+            .get("http://0.0.0.0:5000/api/v1/tournaments")
+            .then(res => {
+              const tournament = (res && res.data && res.data.data) || [];
+              updateTournament(tournament);
+            })
+            .catch(err => err);
+        })
+        .catch(err => err);
+    }
+  };
+
+  if (isEditable === true) {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          edit();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter Tournament Title"
+          onChange={e => {
+            handleChangeTitle(e);
+          }}
+        />
+        <InputMask
+          mask="9999/99/99"
+          placeholder="Start Date YYYY/MM/DD"
+          onChange={e => {
+            handleChangeStartDate(e);
+          }}
+        />
+        <InputMask
+          mask="9999/99/99"
+          placeholder="Finish Date YYYY/MM/DD"
+          onChange={e => {
+            handleChangeFinishDate(e);
+          }}
+        />
+        <button>Save</button>
+      </form>
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        onClick={() => {
+          handleEditClick();
+        }}
+      >
+        Edit
+      </button>
+    );
+  }
+}
+
+export default EditData;
