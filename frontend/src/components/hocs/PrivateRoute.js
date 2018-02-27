78d1a251d9c7e6d8d968778a26dee093f897cb1c
Arun Tamang <aruntamang3791@gmail.com>
2018-02-27 12:58:15 +0545

    SPORTS-53 Use spread operator for transferring props in PrivateRoute

--- frontend/src/components/hocs/PrivateRoute.js
+++ frontend/src/components/hocs/PrivateRoute.js
@@ -4,14 +4,14 @@ import { Route, Redirect } from 'react-router';
 import * as routes from '../../constants/routes';
 
 const PrivateRoute = props => {
-  const { path, Component, showToaster, isAuthenticated } = props;
+  const { path, Component, isAuthenticated, ...otherProps } = props;
 
   return (
     <Route
       path={path}
       render={routerProps =>
         isAuthenticated ? (
-          <Component showToaster={showToaster} />
+          <Component {...otherProps} />
         ) : (
           <Redirect
             to={{
