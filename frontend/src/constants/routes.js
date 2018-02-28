9b12ff58d68e4a817dc9e9bee64f0e11ad7736f6
Arun Tamang <aruntamang3791@gmail.com>
2018-02-28 10:55:20 +0545

    SPORTS-53 Rename route variable

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -2,4 +2,4 @@ export const ROOT = '/';
 export const ADMIN = '/admin';
 export const LOGIN = '/login';
 export const TOURNAMENT_TREE = '/tournament/tree';
-export const TOURNAMENT_FIXTURE_OVERVIEW = '/tournament/fixture-overview';
+export const FIXTURE_OVERVIEW = '/tournament/fixture-overview';
