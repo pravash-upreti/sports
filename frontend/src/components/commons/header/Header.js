23eacf39bf3e6a133e6e24e324958ac5692a6bed
ishanliv99 <ishanliv99@gmail.com>
2018-03-02 19:07:21 +0545

    SPORTS-77 Add Link instead of a tag

--- frontend/src/components/commons/header/Header.js
+++ frontend/src/components/commons/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../../../public/assets/images/sports-logo.png';
 
@@ -8,9 +9,9 @@ const Header = props => (
   <div className="header-main-container">
     <div className="header-content-container">
       <div className="logo-container">
-        <a href={ROOT}>
+        <Link to={ROOT}>
           <img src={logo} alt="Sports logo" />
-        </a>
+        </Link>
       </div>
       <div className="tournament-description-container">
         <span>Chess Tournament 2018</span>
