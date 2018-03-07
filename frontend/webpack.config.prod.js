0f34ab2a2f3c49601836d826e5070fc8522602b1
Aviskar KC <aviskarkc10@gmail.com>
2018-03-07 14:43:59 +0545

    SPORTS-82 Edit production webpack config to load svg images

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -42,11 +42,11 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: 'file-loader?name=[name].[ext]&publicPath=/images/&outputPath=/'
+        use: ['file-loader']
       },
       {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: 'file-loader?name=[name].[ext]&publicPath=/fonts/&outputPath=/'
+        test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: ['file-loader']
       }
     ]
   },
