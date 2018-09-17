d0ef5d392c41afb3c0d1617fbf206c149937ba97
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-09-17 23:13:10 +0545

    Fix: Mark the main nav active on routing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,25 +15,25 @@ const Header = () => (
       </div>
       <ul className="nav nav--primary">
         <li>
-          <NavLink exact to={BASE_ROUTES.FUTSAL} title="Futsal">
+          <NavLink to={BASE_ROUTES.FUTSAL} title="Futsal">
             <FontAwesomeIcon icon={faFutbol} size="lg" />
             <span className="nav__link-text">FUTSAL</span>
           </NavLink>
         </li>
         <li>
-          <NavLink exact to={BASE_ROUTES.CHESS} title="Chess">
+          <NavLink to={BASE_ROUTES.CHESS} title="Chess">
             <FontAwesomeIcon icon={faChess} size="lg" />
             <span className="nav__link-text">CHESS</span>
           </NavLink>
         </li>
         <li>
-          <NavLink exact to={BASE_ROUTES.CARROM_BOARD} title="Carom-board">
+          <NavLink to={BASE_ROUTES.CARROM_BOARD} title="Carom-board">
             <FontAwesomeIcon icon={faExpand} size="lg" />
             <span className="nav__link-text">CAROM-BOARD</span>
           </NavLink>
         </li>
         <li>
-          <NavLink exact to={BASE_ROUTES.TABLE_TENNIS} title="Table Tennis">
+          <NavLink to={BASE_ROUTES.TABLE_TENNIS} title="Table Tennis">
             <FontAwesomeIcon icon={faTableTennis} size="lg" />
             <span className="nav__link-text">TABLE-TENNIS</span>
           </NavLink>
