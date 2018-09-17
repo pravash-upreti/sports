4e1039cdf4756677a898f8ead4c4a976296e49b5
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-09-17 20:59:44 +0545

    Update score card design to use smaller fonts

--- src/components/common/scoreCard/partials/FixtureDate.js
+++ src/components/common/scoreCard/partials/FixtureDate.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 const FixtureDate = props => (
   <div className="other-info date">
-    <p className="game-time">{props.fixtureDate.time}</p>
     <p className="game-time">{props.fixtureDate.weekDay}</p>
     <p>{props.fixtureDate.date}</p>
+    <p className="game-time">{props.fixtureDate.time}</p>
   </div>
 );
 
