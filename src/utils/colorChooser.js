ca0143f0620926bf7904ffd13878f8e7c20a754e
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-09-19 20:57:31 +0545

    Use pastel colors for logo backgrounds

--- src/utils/colorChooser.js
+++ src/utils/colorChooser.js
@@ -2,7 +2,7 @@ import colors from '../constants/colors';
 
 export function getOneRandomColor() {
   const min = 0;
-  const max = colors.length - 1;
+  const max = colors.length;
   const randomNumber = Math.floor(Math.random() * (max - min) + min);
 
   return colors[randomNumber];
