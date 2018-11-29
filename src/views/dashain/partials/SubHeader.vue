d20578a98b28e1e8a08225a41280092168e3a2fd
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-11-29 20:45:24 +0545

    #46 Reformat code

--- src/views/dashain/partials/SubHeader.vue
+++ src/views/dashain/partials/SubHeader.vue
@@ -3,52 +3,22 @@
     <div class="sub-header">
       <ul class="nav nav--secondary container">
         <li>
-          <router-link
-            :to="futsalRoutes.HOME"
-            exact-active-class="active"
-            exact
-          >Home
-          </router-link>
+          <router-link :to="futsalRoutes.HOME" exact-active-class="active" exact>Home</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.RESULTS"
-            exact-active-class="active"
-            exact
-          >Results
-          </router-link>
+          <router-link :to="futsalRoutes.RESULTS" exact-active-class="active" exact>Results</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.FIXTURES"
-            exact-active-class="active"
-            exact
-          >Fixtures
-          </router-link>
+          <router-link :to="futsalRoutes.FIXTURES" exact-active-class="active" exact>Fixtures</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.POINTS"
-            exact-active-class="active"
-            exact
-          >Points
-          </router-link>
+          <router-link :to="futsalRoutes.POINTS" exact-active-class="active" exact>Points</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.STATS"
-            exact-active-class="active"
-            exact
-          >Stats
-          </router-link>
+          <router-link :to="futsalRoutes.STATS" exact-active-class="active" exact>Stats</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.TEAMS"
-            exact-active-class="active"
-            exact
-          >Teams
-          </router-link>
+          <router-link :to="futsalRoutes.TEAMS" exact-active-class="active" exact>Teams</router-link>
         </li>
       </ul>
     </div>
@@ -56,33 +26,33 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
-  import { OTHER_GAMES_ROUTES } from '../../../constants/routes';
+import { OTHER_GAMES_ROUTES } from '../../../constants/routes';
 
-  @Component
-  export default class SubHeader extends Vue {
-    private futsalRoutes: object = Object.freeze(OTHER_GAMES_ROUTES.DASHAIN_CUP);
-    private header: any;
-    private stickyOffset: any;
+@Component
+export default class SubHeader extends Vue {
+  private futsalRoutes: object = Object.freeze(OTHER_GAMES_ROUTES.DASHAIN_CUP);
+  private header: any;
+  private stickyOffset: any;
 
-    public mounted() {
-      this.header = document.getElementById('subHeader');
-      this.stickyOffset = this.header.offsetTop;
+  public mounted() {
+    this.header = document.getElementById('subHeader');
+    this.stickyOffset = this.header.offsetTop;
 
-      window.onscroll = this.handleScroll();
-    }
+    window.onscroll = this.handleScroll();
+  }
 
-    public handleScroll() {
-      return this.updateOffset;
-    }
+  public handleScroll() {
+    return this.updateOffset;
+  }
 
-    private updateOffset() {
-      if (window.pageYOffset > this.stickyOffset) {
-        this.header.classList.add('sticky');
-      } else {
-        this.header.classList.remove('sticky');
-      }
+  private updateOffset() {
+    if (window.pageYOffset > this.stickyOffset) {
+      this.header.classList.add('sticky');
+    } else {
+      this.header.classList.remove('sticky');
     }
   }
+}
 </script>
--- src/views/futsal/partials/SubHeader.vue
+++ src/views/futsal/partials/SubHeader.vue
@@ -3,52 +3,22 @@
     <div id="subHeader" class="sub-header">
       <ul class="nav nav--secondary container">
         <li>
-          <router-link
-            :to="futsalRoutes.HOME"
-            exact-active-class="active"
-            exact
-          >Home
-          </router-link>
+          <router-link :to="futsalRoutes.HOME" exact-active-class="active" exact>Home</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.RESULTS"
-            exact-active-class="active"
-            exact
-          >Results
-          </router-link>
+          <router-link :to="futsalRoutes.RESULTS" exact-active-class="active" exact>Results</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.FIXTURES"
-            exact-active-class="active"
-            exact
-          >Fixtures
-          </router-link>
+          <router-link :to="futsalRoutes.FIXTURES" exact-active-class="active" exact>Fixtures</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.POINTS"
-            exact-active-class="active"
-            exact
-          >Points
-          </router-link>
+          <router-link :to="futsalRoutes.POINTS" exact-active-class="active" exact>Points</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.STATS"
-            exact-active-class="active"
-            exact
-          >Stats
-          </router-link>
+          <router-link :to="futsalRoutes.STATS" exact-active-class="active" exact>Stats</router-link>
         </li>
         <li>
-          <router-link
-            :to="futsalRoutes.TEAMS"
-            exact-active-class="active"
-            exact
-          >Teams
-          </router-link>
+          <router-link :to="futsalRoutes.TEAMS" exact-active-class="active" exact>Teams</router-link>
         </li>
       </ul>
     </div>
@@ -56,33 +26,33 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
-  import { FUTSAL_ROUTES } from '../../../constants/routes';
+import { FUTSAL_ROUTES } from '../../../constants/routes';
 
-  @Component
-  export default class SubHeader extends Vue {
-    public futsalRoutes: object = FUTSAL_ROUTES;
-    private header: any;
-    private stickyOffset: any;
+@Component
+export default class SubHeader extends Vue {
+  public futsalRoutes: object = FUTSAL_ROUTES;
+  private header: any;
+  private stickyOffset: any;
 
-    public mounted() {
-      this.header = document.getElementById('subHeader');
-      this.stickyOffset = this.header.offsetTop;
+  public mounted() {
+    this.header = document.getElementById('subHeader');
+    this.stickyOffset = this.header.offsetTop;
 
-      window.onscroll = this.handleScroll();
-    }
+    window.onscroll = this.handleScroll();
+  }
 
-    public handleScroll() {
-      return this.updateOffset;
-    }
+  public handleScroll() {
+    return this.updateOffset;
+  }
 
-    private updateOffset() {
-      if (window.pageYOffset > this.stickyOffset) {
-        this.header.classList.add('sticky');
-      } else {
-        this.header.classList.remove('sticky');
-      }
+  private updateOffset() {
+    if (window.pageYOffset > this.stickyOffset) {
+      this.header.classList.add('sticky');
+    } else {
+      this.header.classList.remove('sticky');
     }
   }
+}
 </script>
