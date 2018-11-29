49ae17bfa5211a186ad4e614a5a710c0d316f081
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-11-29 20:40:50 +0545

    #46 Change color of the sub-header

--- src/views/carrom-board/partials/SubHeader.vue
+++ src/views/carrom-board/partials/SubHeader.vue
@@ -1,66 +1,72 @@
 <template>
-  <div id="subHeader" class="sub-header">
-    <ul class="nav nav--secondary container">
-      <li>
-        <router-link 
-          :to="routes.HOME"
-          exact-active-class="active"
-          exact
-        >Home</router-link>
-      </li>
-      <li>
-        <router-link 
-          :to="routes.RESULTS"
-          exact-active-class="active"
-          exact
-        >Results</router-link>
-      </li>
-      <li>
-        <router-link 
-          :to="routes.FIXTURES"
-          exact-active-class="active"
-          exact
-        >Fixtures</router-link>
-      </li>
-      <li>
-        <router-link 
-          :to="routes.TEAMS"
-          exact-active-class="active"
-          exact
-        >Teams</router-link>
-      </li>
-    </ul>
+  <div id="subHeader" class="sub-header-wrapper">
+    <div class="sub-header">
+      <ul class="nav nav--secondary container">
+        <li>
+          <router-link
+            :to="routes.HOME"
+            exact-active-class="active"
+            exact
+          >Home
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            :to="routes.RESULTS"
+            exact-active-class="active"
+            exact
+          >Results
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            :to="routes.FIXTURES"
+            exact-active-class="active"
+            exact
+          >Fixtures
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            :to="routes.TEAMS"
+            exact-active-class="active"
+            exact
+          >Teams
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
 
-import { CARROM_BOARD_ROUTES } from '@/constants/routes';
+  import { CARROM_BOARD_ROUTES } from '@/constants/routes';
 
-@Component
-export default class SubHeader extends Vue {
-  public routes: object = CARROM_BOARD_ROUTES;
-  private header: any;
-  private stickyOffset: any;
+  @Component
+  export default class SubHeader extends Vue {
+    public routes: object = CARROM_BOARD_ROUTES;
+    private header: any;
+    private stickyOffset: any;
 
-  public mounted() {
-    this.header = document.getElementById('subHeader');
-    this.stickyOffset = this.header.offsetTop;
+    public mounted() {
+      this.header = document.getElementById('subHeader');
+      this.stickyOffset = this.header.offsetTop;
 
-    window.onscroll = this.handleScroll();
-  }
+      window.onscroll = this.handleScroll();
+    }
 
-  public handleScroll() {
-    return this.updateOffset;
-  }
+    public handleScroll() {
+      return this.updateOffset;
+    }
 
-  private updateOffset() {
-    if (window.pageYOffset > this.stickyOffset) {
-      this.header.classList.add('sticky');
-    } else {
-      this.header.classList.remove('sticky');
+    private updateOffset() {
+      if (window.pageYOffset > this.stickyOffset) {
+        this.header.classList.add('sticky');
+      } else {
+        this.header.classList.remove('sticky');
+      }
     }
   }
-}
 </script>
