e17c01fe5e271675e64f74c4927d5a3b9afb6886
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-11-29 17:30:26 +0545

    Update deploy config

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -19,10 +19,10 @@ printf "Preparing production build...\n"
 docker-compose up web_build
 
 printf "Preparing docker image...\n"
-docker build -t registry.lftechnology.com/sports:latest -f ./docker/Dockerfile .
+docker build -t registry.lftechnology.com/sports:stable -f ./docker/Dockerfile .
 
 printf "Pushing docker image...\n"
-docker push registry.lftechnology.com/sports:latest
+docker push registry.lftechnology.com/sports:stable
 
 printf "Deploying docker image...\n"
-curl -X POST --user $DEPLOYMENT_USERNAME:$DEPLOYMENT_PASSWORD https://registry.lftechnology.com/deploy/dev.sports.lftechnology.com
+curl -X POST --user $DEPLOYMENT_USERNAME:$DEPLOYMENT_PASSWORD https://registry.lftechnology.com/deploy/sports.lftechnology.com
