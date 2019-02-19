819e41e8b63c6112f3906d87e90c59cef390f99e
Sanjeev Kumar Pandit <sanjeevpandit@lftechnology.com>
2019-02-19 16:28:45 +0000

    Merge dev to master

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -5,6 +5,17 @@ import invertedLogo from './images/logo-inverted.svg';
 import redCardIcon from './images/futsal/red-card.png';
 import footballIcon from './images/futsal/football.png';
 import yellowCardIcon from './images/futsal/yellow-card.png';
+import footballAssistIcon from './images/futsal/football-assist.png';
 import doubleYellowCardIcon from './images/futsal/double-yellow-cards.png';
 
-export { logo, logoSVG, futsalSVG, invertedLogo, redCardIcon, footballIcon, yellowCardIcon, doubleYellowCardIcon };
+export {
+  logo,
+  logoSVG,
+  futsalSVG,
+  invertedLogo,
+  redCardIcon,
+  footballIcon,
+  yellowCardIcon,
+  footballAssistIcon,
+  doubleYellowCardIcon
+};
--- src/components/common/score-card/partials/ScoreCardDesktop.vue
+++ src/components/common/score-card/partials/ScoreCardDesktop.vue
@@ -4,26 +4,41 @@
     :class="{ 'score-card--cancelled': isFixtureCancelled }"
     @click="showFixtureModal"
   >
+    <!-- Fixture date info -->
     <div class="other-info-wrapper other-info-wrapper--left">
       <span class="other-info-wrapper__day">{{ fixtureDate.day }}</span>
       <span class="other-info-wrapper__date">{{ fixtureDate.date }}</span>
       <span class="other-info-wrapper__time">{{ fixtureDate.time }}</span>
     </div>
+    <!-- Home team -->
     <div class="team-wrapper team-wrapper--home">
-      <div class="fixture-team-logo"><TeamLogo :team="fixture.homeTeam"/></div>
+      <div class="fixture-team-logo">
+        <TeamLogo :team="fixture.homeTeam"/>
+      </div>
       <p class="fixture-team-name">{{ getTeamInfo(fixture.homeTeam).name }}</p>
     </div>
+    <!-- Result -->
     <div v-if="isFixtureCancelled" class="score-wrapper score-wrapper--cancelled">CANCELLED</div>
     <div v-else-if="isFixturePlayed" class="score-wrapper">
-      <span class="score-wrapper__home">{{ fixture.homeTeamScore }}</span>
+      <span class="score-wrapper__home">
+        <i v-if="isHomeTeamWinnerForNoScore" class="fas fa-trophy"/>
+        {{ fixture.homeTeamScore }}
+      </span>
       <span class="score-wrapper__dot">:</span>
-      <span class="score-wrapper__away">{{ fixture.awayTeamScore }}</span>
+      <span class="score-wrapper__away">
+        {{ fixture.awayTeamScore }}
+        <i v-if="isAwayTeamWinnerForNoScore" class="fas fa-trophy"/>
+      </span>
     </div>
     <div v-else class="score-wrapper score-wrapper--versus">vs</div>
+    <!-- Away team -->
     <div class="team-wrapper team-wrapper--away">
       <p class="fixture-team-name">{{ getTeamInfo(fixture.awayTeam).name }}</p>
-      <div class="fixture-team-logo"><TeamLogo :team="fixture.awayTeam"/></div>
+      <div class="fixture-team-logo">
+        <TeamLogo :team="fixture.awayTeam"/>
+      </div>
     </div>
+    <!-- Other info -->
     <div class="other-info-wrapper other-info-wrapper--right">
       <span
         v-if="fixture.categoryType && fixture.categoryType.length"
@@ -33,20 +48,17 @@
         v-if="fixture.round && fixture.round.length"
         class="other-info-wrapper__round"
       >{{ fixture.round }}</span>
-      <span
-        v-if="showMatchNumber"
-        class="other-info-wrapper__match"
-      >Match {{ fixture.id }}</span>
+      <span v-if="showMatchNumber" class="other-info-wrapper__match">Match {{ fixture.id }}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { FixtureInterface, TeamInterface } from '@/interfaces/interfaces';
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import { getTeamInfo } from '@/services/TeamService';
 import TeamLogo from '@/components/common/team-logo/TeamLogo.vue';
+import { FixtureInterface, TeamInterface } from '@/interfaces/interfaces';
 import { getFixtureDate, isFixturePlayed, isFixtureCancelled } from '@/services/FixtureService';
 
 @Component({
@@ -76,6 +88,22 @@ export default class ScoreCardDesktop extends Vue {
     return !(
       (this.fixture.round && this.fixture.round.length) ||
       (this.fixture.categoryType && this.fixture.categoryType.length)
+    );
+  }
+
+  get isHomeTeamWinnerForNoScore() {
+    return (
+      isFixturePlayed(this.fixture) &&
+      this.fixture.winnerTeam &&
+      this.fixture.winnerTeam.id === this.fixture.homeTeam.id
+    );
+  }
+
+  get isAwayTeamWinnerForNoScore() {
+    return (
+      isFixturePlayed(this.fixture) &&
+      this.fixture.winnerTeam &&
+      this.fixture.winnerTeam.id === this.fixture.awayTeam.id
     );
   }
 }
