958b8f5e43de3f5037fd984e38a8eee4ccd7e156
Sanjeev Kumar Pandit <sanjeevpandit@lftechnology.com>
2019-06-17 10:24:41 +0545

    Add admin-app

---
+++ admin-app/src/components/Logo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a href="/" class="logo-wrapper">
+      <img v-bind:src="logo" class="logo" alt="sports" />
+    </a>
+  </div>
+</template>
+
+<script>
+import invertedLogo from '@/assets/images/logo.svg';
+
+export default {
+  name: 'Logo',
+  data() {
+    return {
+      logo: invertedLogo
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.logo-wrapper {
+  display: inline-block;
+
+  .logo {
+    display: inline-block;
+    width: 200px;
+  }
+}
+</style>
---
+++ admin-app/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header>
+        <Logo/>
+      </a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/Logo.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Logo
+  }
+};
+</script>
